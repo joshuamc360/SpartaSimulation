f51b78e1fb316304b143a591f6800055d262defc
JoeAH2000 <joehilton00@gmail.com>
2021-08-24 14:26:11 +0100

    Created Trainee Class

---
+++ Sparta-Sumulator/src/com/sparta/controller/model/Trainee.java
@@ -0,0 +1,13 @@
+package com.sparta.controller.model;
+
+public class Trainee implements TraineeDTO {
+    @Override
+    public String getState() {
+        return null;
+    }
+
+    @Override
+    public void setState(String state) {
+
+    }
+}
