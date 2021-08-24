6476db6ea676c4738b942d47decc8a7042c717df
Sogurung16 <Sogurung16@gmail.com>
2021-08-24 12:09:40 +0100

    added necessary imports

--- Sparta-Sumulator/com.sparta/src/TrainingCentreManager.java
+++ Sparta-Sumulator/com.sparta/src/TrainingCentreManager.java
@@ -1,3 +1,5 @@
+import model.TrainingCentre;
+
 import java.util.ArrayList;
 
 public interface TrainingCentreManager {
--- Sparta-Sumulator/com.sparta/src/WaitingListManager.java
+++ Sparta-Sumulator/com.sparta/src/WaitingListManager.java
@@ -1,3 +1,5 @@
+import model.TraineeDTO;
+
 import java.util.ArrayList;
 
 public interface WaitingListManager {
