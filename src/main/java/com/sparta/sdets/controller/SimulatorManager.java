5594339e10f831a8b9aa67ce10d0b24363f5fd94
CPlatts <cplatts@spartaglobal.com>
2021-08-25 14:07:13 +0100

    Added outline for class functionality

--- src/main/java/com/sparta/sdets/controller/SimulatorManager.java
+++ src/main/java/com/sparta/sdets/controller/SimulatorManager.java
@@ -1,27 +1,63 @@
 package com.sparta.sdets.controller;
 
+import com.sparta.sdets.view.Displayable;
+import com.sparta.sdets.view.Inputable;
+
 public class SimulatorManager {
 
+    Inputable inputManager;
+    Displayable displayManager;
+    TrainingCentreManagerImpl trainingCentreManager = new TrainingCentreManagerImpl();
+    WaitingListManager waitingListManager;
+
+    int remainingSimulationDuration;
+    boolean monthlyOutput;
     //TODO: Take input
         //Duration
         //Output frequency
 
-    //TODO: Main loop
-        //Generating trainees
-        //Generating training centres
-        //Allocating trainees to available centres
-        //Allocating trainees to waiting list
-        //Richard
-            //Checking training centres
-            //Closing training centres
+    remainingSimulationDuration = inputManager.getSimulationDuration();
 
-    //TODO: End of month - output
+    //TODO: Main loop
+    for(int i = remainingSimulationDuration; i > 0; i --) {
+
+        //Generating trainees
+        //Allocating trainees to waiting list
+        waitingListManager.generateTrainees();
+
+
+        //Generating training centres
+
+        if(2 months passed) {
+            trainingCentreManager.createCentre();
+        }
+
+        //Allocating trainees to available centres
+        trainingCentreManager.allocateTrainees(waitingListManager.getList());
+
+        //Richard
+        //Checking training centres
+        //Closing training centres
+
+
+        if(monthlyOutput) {
+            //TODO: End of month - output
+            //Number of open centres
+            //Number of full centres
+            //Number of trainees currently training
+            //Number of trainees on waiting list
+            //Number of closed centres
+
+            displayManager.outputAll();
+
+        }
+    }
 
     //TODO: End of simulation - output
-        //Number of open centres
-        //Number of full centres
-        //Number of trainees currently training
-        //Number of trainees on waiting list
-        //Number of closed centres
-
+    //Number of open centres
+    //Number of full centres
+    //Number of trainees currently training
+    //Number of trainees on waiting list
+    //Number of closed centres
+    displayManager.outputAll();
 }
