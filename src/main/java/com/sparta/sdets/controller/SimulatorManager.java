253ae4dc7470a42cae426554157ae43a32f8fb92
CPlatts <cplatts@spartaglobal.com>
2021-08-25 13:45:50 +0100

    Added comments outlining what needs to be done for this class

---
+++ src/main/java/com/sparta/sdets/controller/SimulatorManager.java
@@ -0,0 +1,27 @@
+package com.sparta.sdets.controller;
+
+public class SimulatorManager {
+
+    //TODO: Take input
+        //Duration
+        //Output frequency
+
+    //TODO: Main loop
+        //Generating trainees
+        //Generating training centres
+        //Allocating trainees to available centres
+        //Allocating trainees to waiting list
+        //Richard
+            //Checking training centres
+            //Closing training centres
+
+    //TODO: End of month - output
+
+    //TODO: End of simulation - output
+        //Number of open centres
+        //Number of full centres
+        //Number of trainees currently training
+        //Number of trainees on waiting list
+        //Number of closed centres
+
+}
