b6dcab7d25d6ff9e79c4ec4098fd46a81867fa00
CPlatts <cplatts@spartaglobal.com>
2021-08-25 14:45:32 +0100

    Added calls to UI

--- src/main/java/com/sparta/sdets/controller/SimulatorManager.java
+++ src/main/java/com/sparta/sdets/controller/SimulatorManager.java
@@ -1,30 +1,42 @@
 package com.sparta.sdets.controller;
 
+import com.sparta.sdets.model.Trainee;
+import com.sparta.sdets.view.DisplayManager;
 import com.sparta.sdets.view.Displayable;
 import com.sparta.sdets.view.Inputable;
+
+import java.util.List;
 
 public class SimulatorManager {
 
     Inputable inputManager;
-    Displayable displayManager;
+    Displayable displayManager = new DisplayManager();
     TrainingCentreManagerImpl trainingCentreManager = new TrainingCentreManagerImpl();
     WaitingListManager waitingListManager;
 
+    int simulationDuration;
     int remainingSimulationDuration;
     boolean monthlyOutput;
     //TODO: Take input
         //Duration
         //Output frequency
 
-    remainingSimulationDuration = inputManager.getSimulationDuration();
+    simulationDuration = inputManager.getSimulationDuration();
+    monthlyOutput = inputManager.isMonthlyOutput();
+
+    remainingSimulationDuration = simulationDuration;
+
+    displayManager.printGetMonths(simulationDuration);
 
     //TODO: Main loop
     for(int i = remainingSimulationDuration; i > 0; i --) {
 
         //Generating trainees
         //Allocating trainees to waiting list
-        waitingListManager.generateTrainees();
-
+        List<Trainee> trainees = waitingListManager.generateTrainees();
+        for (Trainee trainee : trainees) {
+            waitingListManager.addToWaitingList(trainee);
+        }
 
         //Generating training centres
 
@@ -48,7 +60,13 @@ public class SimulatorManager {
             //Number of trainees on waiting list
             //Number of closed centres
 
-            displayManager.outputAll();
+            displayManager.printSimulationResults(
+                    trainingCentreManager.getFullCentres().size(),
+                    waitingListManager.getTrainees().size(),
+                    simulationDuration,
+                    trainingCentreManager.getAllTrainingCentreDTOs.size(),
+                    trainingCentreManager.getAvailableCentres().size()
+            );
 
         }
     }
@@ -59,5 +77,11 @@ public class SimulatorManager {
     //Number of trainees currently training
     //Number of trainees on waiting list
     //Number of closed centres
-    displayManager.outputAll();
+    displayManager.printSimulationResults(
+        trainingCentreManager.getFullCentres(),
+        waitingListManager.getTrainees().size(),
+        simulationDuration,
+        trainingCentreManager.getAllTraining.size(),
+        trainingCentreManager.getAvailableCentres()
+    );
 }
