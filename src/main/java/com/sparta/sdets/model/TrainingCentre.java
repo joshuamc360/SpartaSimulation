a798e98bc45b09e3b4c6e835dfe32964716bf592
Sogurung16 <Sogurung16@gmail.com>
2021-08-26 11:51:58 +0100

    refactor and removed addTraineesToCentre (migrated to manager)

--- src/main/java/com/sparta/sdets/model/TrainingCentre.java
+++ src/main/java/com/sparta/sdets/model/TrainingCentre.java
@@ -20,29 +20,12 @@ public class TrainingCentre implements TrainingCentreDTO{
     }
 
     @Override
-    public ArrayList getQueue() {
+    public ArrayList getTraineesList() {
         return this.traineesListInCentre;
     }
 
     @Override
     public void addToQueue(Trainee trainee) {
         traineesListInCentre.add(trainee);
-    }
-
-    public void addTraineesToCentre(){
-        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        int randomNumber = randomNumberGenerator.getRandomNumber(0,20);
-
-        WaitingListImpl waitingListObject = WaitingListImpl.getWaitingListObj();
-        ArrayList<Trainee> traineesInWaitingList = waitingListObject.getTrainees();
-
-        if(randomNumber>traineesInWaitingList.size()){
-            randomNumber = traineesInWaitingList.size();
-        }
-
-        for(int i = 0; i < randomNumber; i++){
-            Trainee trainee = waitingListObject.pop();
-            addToQueue(trainee);
-        }
     }
 }
